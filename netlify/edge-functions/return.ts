899efd43b825f9d04f8ac2a51172a5da8e5e2c2a
Katrina <99131087+notkatrina@users.noreply.github.com>
2024-05-06 22:55:45 -0500

    c

--- netlify/edge-functions/return.ts
+++ netlify/edge-functions/return.ts
@@ -1,19 +1,19 @@
 
 import { HTMLRewriter } from "https://ghuc.cc/worker-tools/html-rewriter/index.ts";
 
-  const url = new URL(request.url);
+  
 export default async (request, context) => {
-
+const url = new URL(request.url);
   // Only run if the `cheese` query parameter is set
   if (!url.searchParams.has("cheese")) {
-    return();
+    return;
   }
 
  // const location = context?.geo?.city;
 
   const response = await context.next();
   const rewriter = new HTMLRewriter()
-    .on("#platter", {
+    .on("#plate", {
       element: (element) => {
         element.setInnerContent("A cheese was returned for a visitor");
       },
--- netlify/edge-functions/returnContext.ts
+++ netlify/edge-functions/returnContext.ts
@@ -1,10 +1,10 @@
 
 import { HTMLRewriter } from "https://ghuc.cc/worker-tools/html-rewriter/index.ts";
 
-  const url = new URL(request.url);
+
 export default async (request, context) => {
   const url = new URL(request.url);
-  // Only run if the `catify` query parameter is set
+  // Only run if the `sponge` query parameter is set
   if (!url.searchParams.has("sponge")) {
     return context.next();
   }
@@ -13,7 +13,6 @@ export default async (request, context) => {
 
   const response = await context.next();
   const rewriter = new HTMLRewriter()
-
     .on("#platter", {
       element: (element) => {
         element.setInnerContent("A sponge was requested for context");
